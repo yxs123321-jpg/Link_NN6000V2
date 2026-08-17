a303e6ad147fc615f788bf849a985dc134773828
yxs123321-jpg <yxs123321@gmail.com>
2026-08-17 17:05:34 +0800

    Update update.sh

--- nn6000v2/scripts/update.sh
+++ nn6000v2/scripts/update.sh
@@ -2,39 +2,30 @@
 set -e
 set -o errexit
 set -o errtrace
-
 error_handler() {
     echo "Error occurred in script at line: ${BASH_LINENO[0]}, command: '${BASH_COMMAND}'"
 }
-
 trap 'error_handler' ERR
-
 REPO_URL=$1
 REPO_BRANCH=$2
 BUILD_DIR=$3
 COMMIT_HASH=$4
-
 # Convert BUILD_DIR to absolute path
 if [[ "$BUILD_DIR" != /* ]]; then
     BUILD_DIR="$(pwd)/$BUILD_DIR"
 fi
-
 FEEDS_CONF="feeds.conf.default"
 GOLANG_REPO="https://github.com/sbwml/packages_lang_golang"
 GOLANG_BRANCH="26.x"
 THEME_SET="argon"
 LAN_ADDR="10.0.0.1"
-
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 BASE_PATH=${BASE_PATH:-$(dirname "$SCRIPT_DIR")}
-
 source "$SCRIPT_DIR/general.sh"
 source "$SCRIPT_DIR/feeds.sh"
 source "$SCRIPT_DIR/packages.sh"
 source "$SCRIPT_DIR/system.sh"
 source "$SCRIPT_DIR/docker.sh"
-
-
 main() {
     clone_repo
     clean_up
@@ -50,6 +41,7 @@ main() {
     clone_oaf
     clone_luci_tailscale
     clone_passwall
+    clone_daed
     install_feeds
     fix_smartdns_makefile
     update_docker_stack
@@ -83,5 +75,4 @@ main() {
     fix_pbr_ip_forward
     fix_quickstart
 }
-
 main "$@"
